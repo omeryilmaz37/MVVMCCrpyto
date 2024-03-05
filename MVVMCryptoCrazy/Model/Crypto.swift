ac583a282bc82b9f797c2802c4c6ae411bc59fe6
Ömer Yılmaz <omerryilmazz.37@gmail.com>
2024-03-05 23:31:39 +0300

    first commit

---
+++ MVVMCryptoCrazy/Model/Crypto.swift
@@ -0,0 +1,13 @@
+//
+//  Crypto.swift
+//  MVVMCryptoCrazy
+//
+//  Created by Ömer Yılmaz on 5.03.2024.
+//
+
+import Foundation
+
+struct Crypto : Decodable{
+    let currency : String
+    let price : String
+}
---
+++ MVVMCryptoCrazy/View/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  MVVMCryptoCrazy
+//
+//  Created by Ömer Yılmaz on 5.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    // m
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        content.text = "Crypto Currency"
+        content.secondaryText = "Crypto Price"
+        cell.contentConfiguration = content
+        return cell
+    }
+}
+
